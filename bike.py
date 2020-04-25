bd1526a35dfe280dc848bf05718c17fbfadc1f72
Wises Keshom <wises.keshom@gmail.com>
2020-04-25 15:47:04 +0700

    initial commit

---
+++ bike.py
@@ -0,0 +1,30 @@
+class bike():
+
+    # Engine configuration
+    engine_v4 = 'V4'
+    engine_i4 = 'I4'
+
+    def __init__(self, engine_type, code):
+        super().__init__()
+
+        self.engine_type = engine_type
+        self.code = code
+        self.engine_status
+
+    def engine_ignition(self, mode):
+        if self.engine_status == mode:
+            return f"The engine is already {self.engine_status}"
+        else:
+            self.engine_status = mode
+        pass
+
+
+class ducati(bike):
+    def __init__(self, version):
+        super().__init__(bike.engine_v4, 'DesmosediciGP')
+        self.version = version
+
+
+class suzuki(bike):
+    def __init__(self):
+        super().__init__(bike.engine_i4, 'GSX-RR')
